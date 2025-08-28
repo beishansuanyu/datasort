3480fd51531fbefb0d25915c8fc1b1154c7be508
beishansuanyu <m18260066390@163.com>
2025-08-28 13:21:32 +0800

    增加写入数字通道时间程序

--- datasort16.py
+++ datasort16.py
@@ -7,6 +7,7 @@ from tkinter import filedialog
 import os
 
 from numpy.f2py.crackfortran import endifs
+from numpy.ma.core import append
 
 root = tk.Tk()
 root.withdraw()
@@ -22,10 +23,17 @@ eof = file.tell()
 file.seek(0,0)
 data = file.read()
 t_temp1 = 0
-fs = 50000
+fs = 20000
 f = open(file_name+"处理后结果.dat", "wb+")
 f0 = open("数字通道1数据.dat",'wb')
 f1 = open("数字通道2数据.dat",'wb')
+f3 = open("数字通道3数据.dat",'wb')
+f4 = open("数字通道4数据.dat",'wb')
+f5 = open("数字通道开始时间.txt",'w+')
+time0 = [0]
+time1 = [0]
+time3 = [0]
+time4 = [0]
 f2 = open("模拟通道数据数据20250728.txt",'w+')
 f2.write('时间')
 for i in range(16):
@@ -34,6 +42,7 @@ f2.write('\n')
 
 
 files = [f,f0,f1,f2]
+times = [time0,time1,time3,time4]
 
 
 def write(item):
@@ -87,35 +96,47 @@ def match_example(item):
 
 
 
-
 pattern = b'\x1a\xcf\xfc'
 n = 0
 while n <eof:
     m = data.find(pattern,n,eof)
     if m != -1:
-        temp = data[m+3]
         if data[m+3] == 16:
             match data[m+4]:
                 case 17:
+                    time0.append(t_temp1)
                     f0.write(data[m+8:m+2184])
                 case 34:
+                    time1.append(t_temp1)
                     f1.write(data[m+8:m+2184])
+                case 51:
+                    time3.append(t_temp1)
+                    f3.write(data[m + 8:m + 2184])
+                case 68:
+                    time4.append(t_temp1)
+                    f4.write(data[m + 8:m + 2184])
                 case _:
                     print(data[m+7])
         elif data[m+3] == 29:
             match_example(data[m+8:m+2184])
             # print(data[m+2182:m+2184])
         f.write(data[m:m + 2184])
-        tongdao = data[m+7]
+        # tongdao = data[m+7]
         # match_example(data[m+7:m+2183])
     else:
         break
     n=m+2184
+    print('\r' + "{:.2f}".format(n / 1000000) + '/' + "{:.2f}".format(eof / 1000000), end='', flush=True)
+i =1
+for t in times:
+    f5.write(f"通道{i}开始时间：{t[1]}"+'\n')
+    i += 1
 
 
 
-while file in files:
-    file.close()
+
+for m in files:
+    m.close()
 
 
 print("helloworld")
